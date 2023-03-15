fc7bf7dda62429e796ea03f609aa1c748d75e8df
Aeroking21 <55886814+Aeroking21@users.noreply.github.com>
2023-03-15 19:18:59 +0000

    Created NotificationHelpers

--- src/Renderer/DrawBlock/NotificationHelper.fs
+++ src/Renderer/DrawBlock/NotificationHelper.fs
@@ -17,3 +17,43 @@ let notificationStyle = Style [
     Right "20px"
     Bottom "20px"
 ]
+
+let errorNotification text closeMsg =
+    fun dispatch ->
+        let close = (fun _ -> dispatch closeMsg)
+        Notification.notification [
+            Notification.Color IsDanger
+            Notification.Props [ notificationStyle ]
+        ] [
+            Delete.delete [ Delete.OnClick close ] []
+            str text
+        ]
+
+let successNotification text closeMsg =
+    fun dispatch ->
+        let close = (fun _ -> dispatch closeMsg)
+        Notification.notification [
+            Notification.Color  Color.IsInfo
+            Notification.Props [ notificationStyle ]
+        ] [
+            Delete.delete [ Delete.OnClick close ] []
+            str text
+        ]
+
+let successPropertiesNotification text = successNotification text ClosePropertiesNotification
+
+
+let viewNotifications model dispatch =
+    let sheetNotifications =
+        match model.Sheet.GetNotifications with
+        | Some msg -> Some <| errorNotification msg CloseDiagramNotification
+        | None -> None
+            
+    [ //model.Notifications.FromDiagram
+      sheetNotifications
+      model.Notifications.FromSimulation
+    ]
+    |> List.tryPick id
+    |> function
+    | Some notification -> notification dispatch
+    | None -> div [] []
